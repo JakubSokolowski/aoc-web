b33cf869dd286e47c93946c9dc1ddd00b87654dd
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-12-02 00:01:02 +0100

    Add 2022-1 solution and add year menu

--- libs/lib-rs/src/aoc_2021/day01.rs
+++ libs/lib-rs/src/aoc_2021/day01.rs
@@ -1,90 +1,65 @@
-#![allow(clippy::unused_unit)]
-
-use wasm_bindgen::prelude::*;
-use crate::utils;
-extern crate web_sys;
-
-macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
-
-#[allow(unused)]
-#[wasm_bindgen]
-pub fn run_part_2(input: &str) -> String {
-  utils::set_panic_hook();
-  let as_num: Vec<usize> = input.split('\n').filter(|l| !l.is_empty()).map(|i| i.parse::<usize>().unwrap()).collect();
-  let count_sliding = count_sliding_increased(&as_num);
-  log!("Got count: {}", count_sliding);
-  count_sliding.to_string()
+pub fn run_first(input: &str) -> String {
+    let as_num: Vec<usize> = input
+        .split('\n')
+        .filter(|l| !l.is_empty())
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect();
+    let count_sliding = count_sliding_increased(&as_num);
+    count_sliding.to_string()
 }
 
 pub fn count_increased(input: &[usize]) -> usize {
-  input
-    .windows(2)
-    .map(|w| if w[1] > w[0] { 1 } else { 0 })
-    .sum()
+    input.windows(2).map(|w| usize::from(w[1] > w[0])).sum()
 }
 
-#[allow(unused)]
-#[wasm_bindgen]
-pub fn run_part_1(input: &str) -> String {
-  utils::set_panic_hook();
-  let as_num: Vec<usize> = input
-    .split('\n')
-    .filter(|l| !l.is_empty())
-    .map(|i| {
-      i.parse::<usize>().unwrap()
-    })
-    .collect();
-  let count = count_increased(&as_num);
-  log!("Got count: {}", count);
-  count.to_string()
+pub fn run_second(input: &str) -> String {
+    let as_num: Vec<usize> = input
+        .split('\n')
+        .filter(|l| !l.is_empty())
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect();
+    let count = count_increased(&as_num);
+    count.to_string()
 }
 
 pub fn count_sliding_increased(input: &[usize]) -> usize {
-  input
-    .windows(4)
-    .map(|w| {
-      let prev = w[0] + w[1] + w[2];
-      let curr = w[1] + w[2] + w[3];
-      if curr > prev {
-        1
-      } else {
-        0
-      }
-    })
-    .sum()
+    input
+        .windows(4)
+        .map(|w| {
+            let prev = w[0] + w[1] + w[2];
+            let curr = w[1] + w[2] + w[3];
+            usize::from(curr > prev)
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_counts_increasing_measurements() {
-    // given
-    let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    #[test]
+    fn test_counts_increasing_measurements() {
+        // given
+        let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-    // when
-    let result = count_increased(&measurements);
+        // when
+        let result = count_increased(&measurements);
 
-    // then
-    let expected = 7;
-    assert_eq!(result, expected);
-  }
+        // then
+        let expected = 7;
+        assert_eq!(result, expected);
+    }
 
-  #[test]
-  fn test_counts_sliding_window_increasing_measurements() {
-    // given
-    let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    #[test]
+    fn test_counts_sliding_window_increasing_measurements() {
+        // given
+        let measurements = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-    // when
-    let result = count_sliding_increased(&measurements);
+        // when
+        let result = count_sliding_increased(&measurements);
 
-    // then
-    let expected = 5;
-    assert_eq!(result, expected);
-  }
+        // then
+        let expected = 5;
+        assert_eq!(result, expected);
+    }
 }
