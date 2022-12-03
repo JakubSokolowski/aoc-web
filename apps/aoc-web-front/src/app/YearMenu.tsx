40a60dbfa83b67065bf600293bd2f5d5da558220
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-12-03 17:23:43 +0100

    Add pathname routing for year/day

---
+++ apps/aoc-web-front/src/app/YearMenu.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import { ProblemInfo, SolvedYearProblems } from './models';
+
+export const YearMenu: FC<{
+    selectedProblem: ProblemInfo;
+    data: SolvedYearProblems;
+    onProblemClick: (info: ProblemInfo) => void;
+}> = ({ data, onProblemClick, selectedProblem }) => {
+    const { year, days } = data;
+
+    return (
+        <div>
+            <div className="accent">{year}</div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                }}
+            >
+                {days.map((d) => {
+                    const selected =
+                        year === selectedProblem.year &&
+                        d.value === selectedProblem.day.value;
+                    return (
+                        <div key={d.value}>
+                            <button
+                                className={`problem-button ${
+                                    selected && 'problem-selected'
+                                }`}
+                                onClick={() => onProblemClick({ year, day: d })}
+                            >
+                                [{d.value}]
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
---
+++ apps/aoc-web-front/src/app/solved-problems.ts
@@ -0,0 +1,45 @@
+import { SolvedYearProblems } from './models';
+
+export const solvedProblems: SolvedYearProblems[] = [
+    {
+        year: 2021,
+        days: [
+            {
+                value: 1,
+                firstMessage: 'Num measurements larger then previous:',
+                secondMessage:
+                    'Num measurements larger then previous in 3-wide sliding window:',
+            },
+            {
+                value: 2,
+                firstMessage: 'Total area:',
+                secondMessage: 'Total aim area:',
+            },
+            {
+                value: 3,
+                firstMessage: 'Power consumption:',
+                secondMessage: 'Life support rating:',
+            },
+        ],
+    },
+    {
+        year: 2022,
+        days: [
+            {
+                value: 1,
+                firstMessage: 'Total calories top Elf is carrying:',
+                secondMessage: 'Total calories top 3 Elves are carrying:',
+            },
+            {
+                value: 2,
+                firstMessage: 'Total score for strategy:',
+                secondMessage: 'Total score for strategy:',
+            },
+            {
+                value: 3,
+                firstMessage: 'Sum of priorities:',
+                secondMessage: 'Sum of priorities:',
+            },
+        ],
+    },
+];
