d804e9d756c78ce1c993192a34bace301f1a8e49
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-12-04 16:15:09 +0100

    Add 2021-6 solution

--- libs/lib-rs/src/lib.rs
+++ libs/lib-rs/src/lib.rs
@@ -50,6 +50,8 @@ pub fn run(year: usize, day: usize, part: Part, input: &str) -> String {
         (2021, 4, Part::Second) => aoc_2021::day04::run_second(input),
         (2021, 5, Part::First) => aoc_2021::day05::run_first(input),
         (2021, 5, Part::Second) => aoc_2021::day05::run_second(input),
+        (2021, 6, Part::First) => aoc_2021::day06::run_first(input),
+        (2021, 6, Part::Second) => aoc_2021::day06::run_second(input),
 
         // 2022
         (2022, 1, Part::First) => aoc_2022::day01::run_first(input),
