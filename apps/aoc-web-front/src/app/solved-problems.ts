ead9c0fed7667abb8586b82c4765665a1730faf9
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-12-04 17:20:31 +0100

    Add 2021-10 solution

--- apps/aoc-web-front/src/app/solved-problems.ts
+++ apps/aoc-web-front/src/app/solved-problems.ts
@@ -8,44 +8,54 @@ export const solvedProblems: SolvedYearProblems[] = [
                 value: 1,
                 firstMessage: 'Num measurements larger then previous:',
                 secondMessage:
-                    'Num measurements larger then previous in 3-wide sliding window:',
+                    'Num measurements larger then previous in 3-wide sliding window:'
             },
             {
                 value: 2,
                 firstMessage: 'Total area:',
-                secondMessage: 'Total aim area:',
+                secondMessage: 'Total aim area:'
             },
             {
                 value: 3,
                 firstMessage: 'Power consumption:',
-                secondMessage: 'Life support rating:',
+                secondMessage: 'Life support rating:'
             },
             {
                 value: 4,
                 firstMessage: 'First winner score:',
-                secondMessage: 'Last winner score:',
+                secondMessage: 'Last winner score:'
             },
             {
                 value: 5,
                 firstMessage: 'Number of overlapping points:',
-                secondMessage: 'Number of overlapping points with diagonal:',
+                secondMessage: 'Number of overlapping points with diagonal:'
             },
             {
                 value: 6,
                 firstMessage: 'Population size after 80 days:',
-                secondMessage: 'Population size after 256 days:',
+                secondMessage: 'Population size after 256 days:'
             },
             {
                 value: 7,
                 firstMessage: 'Total fuel spend:',
-                secondMessage: 'Total fuel spend:',
+                secondMessage: 'Total fuel spend:'
             },
             {
                 value: 8,
                 firstMessage: 'Num times 1,4,7,8 appear:',
-                secondMessage: 'Output sum:',
+                secondMessage: 'Output sum:'
             },
-        ],
+            {
+                value: 9,
+                firstMessage: 'Heightmap risk:',
+                secondMessage: 'Largest basin product:'
+            },
+            {
+                value: 10,
+                firstMessage: 'Syntax error score:',
+                secondMessage: 'Line completion score:'
+            }
+        ]
     },
     {
         year: 2022,
@@ -53,23 +63,23 @@ export const solvedProblems: SolvedYearProblems[] = [
             {
                 value: 1,
                 firstMessage: 'Total calories top Elf is carrying:',
-                secondMessage: 'Total calories top 3 Elves are carrying:',
+                secondMessage: 'Total calories top 3 Elves are carrying:'
             },
             {
                 value: 2,
                 firstMessage: 'Total score for strategy:',
-                secondMessage: 'Total score for strategy:',
+                secondMessage: 'Total score for strategy:'
             },
             {
                 value: 3,
                 firstMessage: 'Sum of priorities:',
-                secondMessage: 'Sum of priorities:',
+                secondMessage: 'Sum of priorities:'
             },
             {
                 value: 4,
                 firstMessage: 'Count assignments containing:',
-                secondMessage: 'Count assignments overlapping:',
-            },
-        ],
-    },
+                secondMessage: 'Count assignments overlapping:'
+            }
+        ]
+    }
 ];
